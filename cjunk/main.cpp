90c23b3140afeb6cd9f66fe4931505f823238311
John Vishnefske <john.vishnefske@viavisolutions.com>
2021-04-26 13:14:56 -0500

    added stub for i2c interface

--- cjunk/main.cpp
+++ cjunk/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <gtest/gtest.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+template<class Impl>
+class I2cInterface: Impl{
+    void read(uint8_t addr){
+
+    }
+};
+
+template<class T>
+void log(T c){std::cout << c;}
+
+class I2cLibImpl{
+    I2cLibImpl() {
+        log("lib constructor");
+    }
+
+    ~I2cLibImpl(){
+        log("lib destructor");
+    }
+
+};
+
+//explicit instanciation.
+class
+
+TEST(tests, i2cTest){
+    std::cout << "i2cTest" << std::endl;
+    I2cInterface
+}
+TEST(tests, test1){
+    ASSERT_TRUE(true);
 }
