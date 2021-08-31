552870b81affe8ad943388a88e27f15ea6c53506
John Vishnefske <jvishnefske@gmail.com>
2021-08-31 15:38:40 -0500

    serialize_string test fails, reduced variant size to 24

--- cjunk/trivialJson/json.cpp
+++ cjunk/trivialJson/json.cpp
@@ -2,6 +2,10 @@
 #include <algorithm>
 #include <ostream>
 #include <sstream>
+#include <vector>
+#include <map>
+#include <string>
+
 
 // todo: add operator<< for each serialization type bson, json, etc.
 // todo convert generators to trivial constructors
@@ -10,28 +14,56 @@
 // todo consider adding bson types where they make sense for other serialization methods decimal128 datetime in milliseconds
 // todo make constexpr operator""json
 // todo is it possble to use compile time schemas and create node.member which links to {"member": value}
-struct JsonVisitor: Node {
-    std::string operator()  ( const std::vector<Node> o)const{
+struct JsonVisitor : Node {
+    std::string operator()(const List o) {
+#if 0
+        if(!o){
+            // for debugging
+            throw std::runtime_error("nullptr in serialization helper operator()");
+            return "nullptr!!!";
+        }
+#endif
         std::ostringstream oss;
         oss << "[";
         bool first = true;
-        for(auto _o: o){
-            if (first){
+        for (auto _o: o) {
+            if (first) {
                 first = false;
-            }else{
+            } else {
                 oss << ",";
             }
-            oss << _o.visit(*this);
-            //oss << std::visit(*this, _o._storage);
+            //oss << "todo 1";//_o.visit(*this);
+            oss << std::visit(*this, _o._storage);
         }
         oss << "]";
         return oss.str();
     }
-    std::string operator()( std::string o)const {
-        return std::string("\"")+o+std::string("\"");
+
+    std::string operator()(std::string o) const {
+        return std::string("\"") + o + std::string("\"");
     }
-    template <typename T>
-    std::string operator() ( T o) const {
+
+
+    //, std::enable_if_t<std::is_same_v<Node::SmallString, CharArray>, bool> = true
+    template<typename CharArray, std::enable_if_t<std::is_same_v<Node::SmallString, CharArray>, bool> = true>
+    std::string operator()(CharArray str) {
+        std::cout << "CharArray" << typeid(str).name() << std::endl;
+
+        // explicitly specify the string length since str is not always null terminated.
+        // note that this will always make std::string::size always return 8 including any optional terminating characters.
+        return std::string("\"") + std::string(str.data(), str.size()).c_str() + std::string("\"");
+    }
+
+
+    template<typename T, std::enable_if_t<std::is_constructible_v<std::string, T>, bool> = true>
+    std::string operator()(T obj) {
+        std::cout << "constructable" << typeid(obj).name() << std::endl;
+        return std::string(obj);
+    }
+
+    template<typename T>
+    std::string operator()(T o) const {
+        std::cout << "to_string" << std::endl;
         return std::to_string(o);
     }
 };
@@ -44,5 +76,6 @@ std::ostream& operator<<(std::ostream &os, Node object) {
 std::string Node::serialize() {
     std::ostringstream oss;
     JsonVisitor v;
-    return visit(v);
+    //return visit(v);
+    return std::visit(v, this->_storage);
 }
--- cjunk/trivialJson/tests/main.cpp
+++ cjunk/trivialJson/tests/main.cpp
@@ -1,13 +1,15 @@
 //#define CATCH_CONFIG_MAIN
 #include <catch2/catch_test_macros.hpp>
 #include "json.hpp"
-
+#include <random>
+#include <array>
 
 using namespace std::string_literals;
 using JsonNode = Node;
-TEST_CASE("serialize_float", "jsonTest"){
+
+TEST_CASE("serialize_float", "jsonTest") {
     JsonNode j(1.1);
-    CHECK(41 > sizeof(JsonNode)); // we don't want this to increase.
+    CHECK(24 >= sizeof(JsonNode)); // we don't want this to increase.
     REQUIRE(j.serialize().length() > 0);
     CHECK("1.100000" == j.serialize());
 }
@@ -30,17 +32,27 @@ TEST_CASE("serialize_int", "jsonTest"){
     CHECK("42" == j.serialize());
 }
 
-TEST_CASE("serialize_list", "jsonTest"){
-    JsonNode j(1,2);
+TEST_CASE("serialize_list", "jsonTest") {
+    JsonNode j(1, 2);
     REQUIRE(j.serialize().length() > 0);
     CHECK("[1,2]" == j.serialize());
 }
 
-#if 0
-BENCHMARK("serialization"){
-	JsonObject o;
-	o="hi"
-	auto result = serialize(o);
-	REQUIRE(o==result);
+// this tests tries to convert a sequence of floats to string, and back.
+// not a trivial task to get exact it turns out.
+// a more typical expectation
+// is to get within epsilon, or one count on the least significatnt decimal
+TEST_CASE("round_trip_conversion", "[!mayfail]") {
+
+    std::random_device r;
+    std::default_random_engine e1(r());
+    std::exponential_distribution<double> rand_e(10.0);
+#pragma omp parallel for
+    for (int i = 0; i < 1000; i = i + 1) {
+        auto initial = rand_e(e1);
+        std::array<char, 1000> buffer{};
+        sprintf_s(buffer.data(), buffer.size(), "%20.20f", initial);
+        auto roundTrip = std::stod(std::string(buffer.data()));
+        REQUIRE(initial == roundTrip);
+    }
 }
-#endif
