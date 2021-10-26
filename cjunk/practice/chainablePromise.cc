1bd6355944a15ac342accf7c1c3d5c7c346935b3
John Vishnefske <john.vishnefske@viavisolutions.com>
2021-10-26 13:09:23 -0500

    chainable promise rev

--- cjunk/practice/chainablePromise.cc
+++ cjunk/practice/chainablePromise.cc
@@ -1,172 +1,111 @@
+// fix compile errors 
 #include <future>
 #include <array>
 #include <variant>
 #include <algorithm>
 #include <iostream>
+
 template<class T>
-class ChainablePromise{
-    public:
-    ChainablePromise then(std::function<T(T)>);
+class ChainablePromise {
+public:
+    using Runnable = std::function<T(T)>;
+    using FutureReturnType = std::future<T>;
+    using FutureType = std::future<ChainablePromise::FutureReturnType>;
+    using FutureVectorType = std::vector<FutureType>;
+    using FutureVectorReturnType = std::vector<ChainablePromise::FutureReturnType>;
+
+
     // takes same arguments as std::function Runable
     void operator()(T);
-    private:
-    Runnable functor;
+
     // run functor, and call all Children with result
-    void run();
-    // add child functor 
-    ChainablePromise then(Runnable onSuccess){
-        ChainablePromise child;
+    // add child functor
+    ChainablePromise<T> then(Runnable onSuccess)
+    {
+        ChainablePromise<T> child;
         child.functor = onSuccess;
         children.push_back(child);
         return child;
     }
+
+public:
     // run all children
-    void operator()(ChainablePromise::FutureReturnType result){
-        auto result = functor();
-        for(auto& child : children){
+    void operator()(T argument)
+    {
+        auto result = functor(argument);
+        for (auto &child: children) {
             child(result);
         }
     }
-    std::vector<ChainablePromise> children;
-    using Runable=std::function<T(T)>;
-    using FutureReturnType=std::future<T>;
-    using FutureType=std::future<ChainablePromise::FutureReturnType>;
-    using FutureVectorType=std::vector<FutureType>;
-    using FutureVectorReturnType=std::vector<ChainablePromise::FutureReturnType>;
+    // this seems like an interesting direction if it could be implemented
+
+#if 0
+
+public:
     // create a future that will return a vector of futures
     FutureVectorType createFutureVector(T);
+
     // create a future that will return a vector of futures
     FutureVectorReturnType createFutureVectorReturn(T);
+
     // create a future that will return a vector of futures
     FutureReturnType createFutureReturn(T);
-    public:
+
     // create a future that will return a vector of futures
-    FutureVectorType createFutureVector(T){
+    FutureVectorType createFutureVector(T)
+    {
         FutureVectorType futures;
-        for(auto& child : children){
+        for (auto &child: children) {
             futures.push_back(child.createFutureReturn(result));
         }
         return futures;
     }
+
     // create a future that will return a vector of futures
-    FutureVectorReturnType createFutureVectorReturn(T){
+    FutureVectorReturnType createFutureVectorReturn(T)
+    {
         FutureVectorReturnType futures;
-        for(auto& child : children){
+        for (auto &child: children) {
             futures.push_back(child.createFutureReturn(result));
         }
         return futures;
     }
+
     // create a future that will return a vector of futures
-    FutureReturnType createFutureReturn(T){
-        return std::async(std::launch::async, [this, result](){
+    FutureReturnType createFutureReturn(T)
+    {
+        FutureReturnType result;
+        return std::async(std::launch::async, [this, result]() {
             auto result = functor();
-            for(auto& child : children){
+            for (auto &child: children) {
                 child(result);
             }
             return result;
         });
+
     }
+
+#endif
+
+public:
+    Runnable functor;
+private:
+    std::vector <ChainablePromise> children;
 };
-void test_chainable_promise(){
-    ChainablePromise p;
-    p.then([](int i){
+
+void test_chainable_promise()
+{
+    ChainablePromise<int> p;
+    p.then([](int i) {
         return i + 1;
-    }).then([](int i){
+    }).then([](int i) {
         return i + 1;
-    }).then([](int i){
-        return i + 1;
-    }).then([](int i){
-        return i + 1;
-    }).then([](int i){
-        return i + 1;
-    }).then([](int i){
-        return i + 1;
-    }).then([](int i){
-        return i + 1;
-    }).then([](int i){
-        return i + 1;
-    }).then([](int i){
-        std::cout << i;
-}
-test_future_vector(){
-    ChainablePromise p;
-    p.then([](int i){
-        return i + 1;
-    }).then([](int i){
-        return i + 1;
-    }).then([](int i){
-        return i + 1;
-    }).then([](int i){
-        return i + 1;
-    }).then([](int i){
-        return i + 1;
-    }).then([](int i){
-        return i + 1;
-    }).then([](int i){
-        return i + 1;
-    }).then([](int i){
-        return i + 1;
-    }).then([](int i){
-        std::cout << i;
     });
-    auto result = p.createFutureVector(0);
-    for(auto& future : result){
-        std::cout << future.get() << std::endl;
-    }
-}
-test_future_vector_return(){
-    ChainablePromise p;
-    p.then([](int i){
-        return i + 1;
-    }).then([](int i){
-        return i + 1;
-    }).then([](int i){
-        return i + 1;
-    }).then([](int i){
-        return i + 1;
-    }).then([](int i){
-        return i + 1;
-    }).then([](int i){
-        return i + 1;
-    }).then([](int i){
-        return i + 1;
-    }).then([](int i){
-        return i + 1;
-    }).then([](int i){
-        std::cout << i;
-    });
-    auto result = p.createFutureVectorReturn(0);
-    for(auto& future : result){
-        std::cout << future << std::endl;
-    }
-}
-test_future_return(){
-    ChainablePromise p;
-    p.then([](int i){
-        return i + 1;
-    }).then([](int i){
-        return i + 1;
-    }).then([](int i){
-        return i + 1;
-    }).then([](int i){
-        return i + 1;
-    }).then([](int i){
-        return i + 1;
-    }).then([](int i){
-        return i + 1;
-    }).then([](int i){
-        return i + 1;
-    }).then([](int i){
-        return i + 1;
-    }).then([](int i){
-        std::cout << i;
-    });
-    auto result = p.createFutureReturn(0);
-    std::cout << result.get() << std::endl;
-}
-int main(){
-    test_future_vector();
-    test_future_vector_return();
-    test_future_return();
+    // check result
+    std::cout << p.createFutureReturn(0).get() << std::endl;
 }
 
+int main()
+{
+    test_chainable_promise();
+}
