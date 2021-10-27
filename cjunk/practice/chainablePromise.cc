a1e359a1d24338e7ff6e1b4c03b36ec5d39c836c
John Vishnefske <john.vishnefske@viavisolutions.com>
2021-10-27 16:15:40 -0500

    add import

--- cjunk/practice/chainablePromise.cc
+++ cjunk/practice/chainablePromise.cc
@@ -1,6 +1,7 @@
 #include <future>
 #include <functional>
-
+#include <iostream>
+#include <vector>
 template<class T>
 class ChainablePromise {
 
@@ -9,6 +10,10 @@ public:
     using FutureReturnType = std::future<T>;
 
     void operator()(T);
+    ChainablePromise then(ChainablePromise<T> promise){
+        m_children.push_back(promise);
+        return promise;
+    }
 
     ChainablePromise then(Runnable onSuccess)
     {
@@ -16,14 +21,19 @@ public:
         m_children.push_back(promise);
         return promise;
     }
-
+    // explicit destructor to disable trivial copy constructor.
+    ~ChainablePromise(){}
     ChainablePromise(Runnable functor);
 
     ChainablePromise();
 
-    FutureReturnType runAsync(T arg)
+    FutureReturnType runAsync(T arg, std::launch policy=std::launch::async)
     {
-        auto future = std::async(std::launch::async, [this, arg]() {
+        std::cout << __FUNCTION__  << ":" << __LINE__ << std::endl;
+
+        auto future = std::async(policy, [this, arg]() {
+            std::cout << __FUNCTION__  << ":" << __LINE__ << std::endl;
+
             return m_functor(arg);
         });
         return future;
@@ -48,27 +58,42 @@ ChainablePromise<T>::ChainablePromise(Runnable functor)
 template<class T>
 void ChainablePromise<T>::operator()(T arg)
 {
+    std::cout << __FUNCTION__  << ":" << __LINE__ << "functor" << (bool)(m_functor) << " type " << m_functor.target_type().name() << std::endl;
+
     auto result = m_functor(arg);
     // exec all children async
     std::vector <FutureReturnType> futures;
     for (auto &child: m_children) {
-        futures.push_back(child.runAsync(result));
+        std::cout << __FUNCTION__  << ":" << __LINE__ << std::endl;
+        child(result);
+        //futures.push_back(child.runAsync(result, std::launch{}));
     }
     for (auto &future: futures) {
+        std::cout << __FUNCTION__  << ":" << __LINE__ << std::endl;
+
         future.wait();
     }
 }
 
 void test_then()
 {
+    std::cout << __FUNCTION__  << ":" << __LINE__ << std::endl;
     ChainablePromise<int> p;
     p.then([](int i) {
         return i + 1;
+    }).then([](int t){
+        std::cout << "##################" << t << std::endl;
+        std::cout << __FUNCTION__  << ":" << __LINE__ << std::endl;
+
+        return 0;
     });
     p(1);
+    //p(2);
+
 }
 
 int main()
 {
+    std::cout << __FUNCTION__  << ":" << __LINE__ << std::endl;
     test_then();
 }
