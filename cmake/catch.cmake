a104892ccd8b43b639320f4b96a112fd08a91bba
John Vishnefske <jvishnefske@gmail.com>
2021-11-03 12:18:07 -0500

    use local catch for compile speed

--- cmake/catch.cmake
+++ cmake/catch.cmake
@@ -5,21 +5,12 @@ set(CATCH_INSTALL_EXTRAS OFF)
 set(CATCH_BUILD_TESTING OFF)
 set(FETCHCONTENT_QUIET OFF)
 
-FetchContent_Declare(
-        Catch2
-        GIT_REPOSITORY https://github.com/catchorg/Catch2.git
-        GIT_TAG v3.0.0-preview3
-)
-#FetchContent_MakeAvailable(Catch2)
-# fetch like this since the default method tries to hit the server even when local content is avaliable.
-FetchContent_GetProperties(Catch2)
-if (NOT EXISTS ${Catch2_BINARY_DIR} OR NOT EXISTS ${Catch2_SOURCE_DIR})
-    FetchContent_populate(Catch2)
-endif ()
-message("unpopulated catch dirs ${Catch2_SOURCE_DIR}, ${Catch2_BINARY_DIR}, pop: ${Catch2_POPULATED}")
-add_subdirectory(${Catch2_SOURCE_DIR} ${Catch2_BINARY_DIR})
-
-
+#FetchContent_Declare(
+#        Catch2
+#        GIT_REPOSITORY https://github.com/catchorg/Catch2.git
+#        GIT_TAG v3.0.0-preview3
+#)
+FetchContent_populate(Catch2)
 include(CTest)
 #include(Catch2)
 #include(ParseAndAddCatchTests)
