44c888c9d85e4d1df9b91a6fdeb230f66ac861e6
John Vishnefske <jvishnefske@gmail.com>
2021-11-03 12:16:57 -0500

    middleware updated

---
+++ middleware/src/first.h
@@ -0,0 +1,21 @@
+//
+// Created by j on 10/23/21.
+//
+
+#ifndef MIDDLEWARE_FIRST_H
+#define MIDDLEWARE_FIRST_H
+
+
+class first {
+    public:
+        void otherFunction(bool value){
+            if(value){
+                std::cout << "true" << std::endl;
+            }else{
+                std::cout << "false" <<std::endl;
+            }
+        }
+};
+
+
+#endif //MIDDLEWARE_FIRST_H
